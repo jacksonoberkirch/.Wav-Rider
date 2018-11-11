4b6a784e335ecd67703987d12460f187fcb66ff2
jacksonoberkirch <35742408+jacksonoberkirch@users.noreply.github.com>
2018-11-11 15:12:13 -0600

    Add files via upload

---
+++ RecordTest/RecordTest/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace RecordTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SourceList = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(384, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(297, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Refreash Sources";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(384, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(297, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(384, 168);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(297, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(384, 222);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(297, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Exit";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // SourceList
+            // 
+            this.SourceList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.SourceList.Location = new System.Drawing.Point(83, 80);
+            this.SourceList.Name = "SourceList";
+            this.SourceList.Size = new System.Drawing.Size(223, 229);
+            this.SourceList.TabIndex = 4;
+            this.SourceList.UseCompatibleStateImageBehavior = false;
+            this.SourceList.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Device";
+            this.columnHeader1.Width = 160;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Channels";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(384, 272);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(297, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "To Wave";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.SourceList);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListView SourceList;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ RecordTest/RecordTest/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecordTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (waveOut != null)
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+
+            }
+
+            if (sourceStream != null)
+            {
+                sourceStream.StopRecording();
+                sourceStream.Dispose();
+                sourceStream = null;
+
+            }
+
+            if (waveWritter != null)
+            {
+                waveWritter.Dispose();
+                waveWritter = null;
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<NAudio.Wave.WaveInCapabilities> sources = new List<NAudio.Wave.WaveInCapabilities>();
+
+            for (int i = 0; i < NAudio.Wave.WaveIn.DeviceCount; i++ )
+            {
+                sources.Add(NAudio.Wave.WaveIn.GetCapabilities(i));
+
+            }
+
+            SourceList.Items.Clear();
+
+            foreach (var source in sources)
+            {
+                ListViewItem item = new ListViewItem(source.ProductName);
+                item.SubItems.Add(new ListViewItem.ListViewSubItem(item, source.Channels.ToString()));
+                SourceList.Items.Add(item);
+
+
+
+
+            }
+
+
+
+
+        }
+
+        private NAudio.Wave.WaveIn sourceStream = null;
+        private NAudio.Wave.DirectSoundOut waveOut = null;
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (SourceList.SelectedItems.Count == 0) return;
+
+            int deviceNumber = SourceList.SelectedItems[0].Index;
+
+            sourceStream = new NAudio.Wave.WaveIn();
+            sourceStream.DeviceNumber = deviceNumber;
+            sourceStream.WaveFormat = new NAudio.Wave.WaveFormat(44100, NAudio.Wave.WaveIn.GetCapabilities(deviceNumber).Channels);
+
+            NAudio.Wave.WaveInProvider waveIn = new NAudio.Wave.WaveInProvider(sourceStream);
+
+            waveOut = new NAudio.Wave.DirectSoundOut();
+            waveOut.Init(waveIn);
+
+            sourceStream.StartRecording();
+            waveOut.Play();
+
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button3_Click(sender, e);
+            this.Close();
+        }
+
+        NAudio.Wave.WaveFileWriter waveWritter = null;
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (SourceList.SelectedItems.Count == 0) return;
+
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Wave File (*.wav)|*.wav;";
+            if (save.ShowDialog() != System.Windows.Forms.DialogResult.OK) return;
+
+
+
+            int deviceNumber = SourceList.SelectedItems[0].Index;
+
+            sourceStream = new NAudio.Wave.WaveIn();
+            sourceStream.DeviceNumber = deviceNumber;
+            sourceStream.WaveFormat = new NAudio.Wave.WaveFormat(44100, NAudio.Wave.WaveIn.GetCapabilities(deviceNumber).Channels);
+
+            sourceStream.DataAvailable += new EventHandler<NAudio.Wave.WaveInEventArgs>(sourceStream_DateAvailable);
+            waveWritter = new NAudio.Wave.WaveFileWriter(save.FileName, sourceStream.WaveFormat);
+
+            sourceStream.StartRecording();
+        }
+
+        private void sourceStream_DateAvailable(object sender,NAudio.Wave.WaveInEventArgs e )
+        {
+            if (waveWritter == null) return;
+
+            waveWritter.WriteData(e.Buffer, 0, e.BytesRecorded);
+            waveWritter.Flush();
+        }
+
+    }
+}
